7edf926ee63b090561cd8e0e2a5d6964a8ec9adc
Kit Jones <174150846+snarelord@users.noreply.github.com>
2024-12-04 15:50:47 +0000

    added greeting function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+// 
+
+function greeting(name) {
+    console.log(`Hello ${name}`);
+}
+
+function greetNames(callback, name1, name2, name3) {
+    callback(name1);
+    callback(name2);
+    callback(name3);
+}
+
+greetNames(greeting, "Kit", "Chris", "Carmen")
